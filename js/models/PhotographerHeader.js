84bc286301cb62329802be4a7677c96edfcaeecc
clemgbld <clementgombauld@hotmail.fr>
2022-03-22 10:53:47 +0100

    img link fixed

--- js/models/PhotographerHeader.js
+++ js/models/PhotographerHeader.js
@@ -25,7 +25,7 @@ class PhotographerHeader {
   }
 
   get picture() {
-    return `./src/photographersIdPhotos/${this._portrait}`;
+    return `/src/photographersIDPhotos/${this._portrait}`;
   }
 
   get price() {
